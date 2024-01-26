17ffa2d9a0f4b2394c012b5bf1919376fa465f52
NotCCG <jonathanbanwart04@outlook.com>
2024-01-26 12:19:36 -0600

    -ORE GENERATION!!!!!

---
+++ src/main/java/net/notccg/yahresurrected/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,39 @@
+package net.notccg.yahresurrected.worldgen;
+
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.notccg.yahresurrected.YouAreHerobrineResurrected;
+
+public class ModBiomeModifiers {
+    public static final ResourceKey<BiomeModifier> ADD_ICE_RUBY_ORE = registerKey("add_ice_ruby_ore");
+    public static final ResourceKey<BiomeModifier> ADD_INVISIBLE_ORE = registerKey("add_invisible_ore");
+
+    public static void bootstrap(BootstapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+        context.register(ADD_ICE_RUBY_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_SNOWY),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.ICE_RUBY_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_INVISIBLE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.INVISIBLE_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(YouAreHerobrineResurrected.MOD_ID, name));
+    }
+}
---
+++ src/main/java/net/notccg/yahresurrected/worldgen/ModConfiguredFeatures.java
@@ -0,0 +1,50 @@
+package net.notccg.yahresurrected.worldgen;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+import net.notccg.yahresurrected.YouAreHerobrineResurrected;
+import net.notccg.yahresurrected.block.ModBlocks;
+
+import java.util.List;
+
+public class ModConfiguredFeatures {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_ICE_RUBY_ORE_KEY = registerKey("ice_ruby_ore");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_INVISIBLE_ORE_KEY = registerKey("invisible_ore");
+
+    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplaceable = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceable = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+
+        List<OreConfiguration.TargetBlockState> overworldIceRubyOres = List.of(OreConfiguration.target(stoneReplaceable,
+                ModBlocks.ICE_RUBY_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceable, ModBlocks.DEEPSLATE_ICE_RUBY_ORE.get().defaultBlockState()));
+
+        register(context, OVERWORLD_ICE_RUBY_ORE_KEY, Feature.ORE, new OreConfiguration(overworldIceRubyOres, 1));
+
+        List<OreConfiguration.TargetBlockState> overworldInvisibleOres = List.of(OreConfiguration.target(stoneReplaceable,
+                ModBlocks.INVISIBLE_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceable, ModBlocks.DEEPSLATE_INVISIBLE_ORE.get().defaultBlockState()));
+
+        register(context, OVERWORLD_INVISIBLE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldInvisibleOres, 2));
+
+
+    }
+
+    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(YouAreHerobrineResurrected.MOD_ID, name));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstapContext<ConfiguredFeature<?, ?>> context,
+                                                                                          ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
+}
