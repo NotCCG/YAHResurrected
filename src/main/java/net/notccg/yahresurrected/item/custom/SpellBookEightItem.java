2361e1c8ead3e0c658c33c1ba9fcaf6d96ff4791
NotCCG <jonathanbanwart04@0utlook.com>
2026-01-23 14:54:16 -0600

    Changes Sync

--- src/main/java/net/notccg/yahresurrected/item/custom/SpellBookEightItem.java
+++ src/main/java/net/notccg/yahresurrected/item/custom/SpellBookEightItem.java
@@ -1,9 +1,13 @@
 package net.notccg.yahresurrected.item.custom;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class SpellBookEightItem extends Item {
     public SpellBookEightItem(Properties pProperties) {
@@ -17,6 +21,10 @@ public class SpellBookEightItem extends Item {
 
     @Override
     public InteractionResult useOn(UseOnContext pContext) {
+        Player player = pContext.getPlayer();
+        Level level = pContext.getLevel();
+        BlockPos clickedBlock = pContext.getClickedPos();
+        BlockState clickedBlockState = level.getBlockState(clickedBlock);
         return super.useOn(pContext);
     }
 }
